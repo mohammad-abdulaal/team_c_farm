23f21db251a00fe1ed3f05bcd317093e0b9cdc6d
louaii <louaimisto@gmail.com>
2021-04-29 08:22:31 +0300

    created scrollview for unicorn listing with sample data

--- frontend/app/components/Home.js
+++ frontend/app/components/Home.js
@@ -1,28 +1,85 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-// import HeaderNotification from './HeaderNotification'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Card } from 'react-native-elements'
 
+class Unicorn extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        {
+          id:1,
+          name: "bellatrix",
+          owner: {
+            id: 111,
+            first_name: "ess",
+            last_name: ""
+          }
+        },
+        {
+          id: 2,
+          name: "arcturus",
+          owner: {
+            id: 111,
+            first_name: "ess",
+            last_name: ""
+          }
+        },
+        {
+          id: 3,
+          name: "vega",
+          owner: {
+            id: "none",
+            first_name: "",
+            last_name: ""
+          },
+        }
+      ]
+    }
+  }
+
+  render(){
+    const entries = this.state.data.map((e, i) => {
+      return(
+        <Card key={i}>
+          <Card.Title>
+            unicorn {i}
+          </Card.Title>
+          <Card.Divider />
+          <Text>{e.name}</Text>
+          <Text>{e.owner.first_name}</Text>
+        </Card>
+      )
+    })
+
+    return(
+      <ScrollView>
+        {entries}
+      </ScrollView>
+    )
+  }
+}
 
 const Home = () => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>This is Home</Text>
+        <View style={styles.appContainer}>
+          {/* <Unicorn style={styles.text}/> */}
+          <Unicorn />
         </View>
-
     )
+
 }
+
 const styles = StyleSheet.create({
-    container:{
+    appContainer:{
         flex:1,
         justifyContent:'center',
         alignItems:'center',
-        backgroundColor:'#76a6ef'
     },
     text:{
         fontSize:20,
         fontWeight:'bold',
-        color:'#fff',
     }
 })
-export default Home;
 
+export default Home
