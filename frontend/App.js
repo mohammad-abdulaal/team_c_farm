6fa435f1a10d79ce28b22974c0aadca811caee6a
louaii <louaimisto@gmail.com>
2021-04-29 07:32:07 +0300

    added search area provider

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,25 @@
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import TabNavigator from './app/navigation/TabNavigator';
 import { Header } from 'react-native-elements';
-import LoginScreen from './screens/login';
 // import { Ionicons } from '@expo/vector-icons';
+
 export default function App() {
-  // return <NavigationContainer>
-  //   <Header>
-  //   {/* <Ionicons name="notifications-circle-outline" size={24} color="black" /> */}
-  //   </Header>
-  //   <TabNavigator/>
-  // </NavigationContainer>
-  return (
-    <LoginScreen/>
-  )
-}
+  return <SafeAreaProvider>
+  <NavigationContainer>
+      <Header>
+      {/* <Ionicons name="notifications-circle-outline" size={24} color="black" /> */}
+      </Header>
+      <TabNavigator/>
+    </NavigationContainer>
+  </SafeAreaProvider>
+    // Login space
+    // return (
+    //   <LoginScreen/>
+    // )
+  }
+
 {/* <Header
     placement="left"
     // leftComponent={{ icon: 'menu', color: '#fff' }}
