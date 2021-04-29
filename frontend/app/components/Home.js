5efeff6abbac86f88b677c7d8d19b96cc33d7f25
louaii <louaimisto@gmail.com>
2021-04-29 10:22:17 +0300

    move unicorn component to its own folder

--- frontend/app/components/Home.js
+++ frontend/app/components/Home.js
@@ -1,100 +1,13 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text, View, Button} from 'react-native'
-import { Card } from 'react-native-elements'
-
-class Unicorn extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      data: [
-        {
-          id:1,
-          name: "bellatrix",
-          owner: {
-            id: 111,
-            first_name: "alice",
-            last_name: ""
-          }
-        },
-        {
-          id: 2,
-          name: "arcturus",
-          owner: {
-            id: 111,
-            first_name: "bob",
-            last_name: ""
-          }
-        },
-        {
-          id: 3,
-          name: "vega",
-          owner: {
-            id: "none",
-            first_name: "charlie",
-            last_name: ""
-          },
-        }
-      ]
-    }
-  }
-
-  render(){
-    const entries = this.state.data.map((e, i) => {
-      return(
-        <Card key={i}>
-          <Card.Title>
-            {e.name}
-          </Card.Title>
-          <Card.Divider />
-          <Text>owner: {e.owner.first_name}</Text>
-          <View style={{flexDirection: 'row'}}>
-          <Button
-            onPress={() => this.handleDelete(i)}
-            title="delete"
-            color="#841584"
-          />
-          <Button
-            onPress={() => this.handleEdit(i)}
-            title="edit"
-            color="#841584"
-          />
-
-          </View>
-        </Card>
-      )
-    })
-
-    return(
-      <ScrollView>
-        {entries}
-      </ScrollView>
-    )
-  }
-
-  // Handle delete unicorn from view
-  handleDelete = (index) => {
-    this.setState(state => {
-      const data = [...state.data]
-      data.splice(index, 1)
-      return { data }
-    })
-  }
-
-  // TODO
-  handleEdit = (index) => {
-    this.setState(state => {
-      const data = [...state.data]
-    })
-  }
-}
+import { StyleSheet, View, } from 'react-native'
+import  { MasterUnicorn } from './Unicorns/index'
 
 const Home = () => {
     return (
         <View style={styles.appContainer}>
-          <Unicorn />
+         <MasterUnicorn />
         </View>
     )
-
 }
 
 const styles = StyleSheet.create({
