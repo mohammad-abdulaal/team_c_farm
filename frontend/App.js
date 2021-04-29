96acd75506035ff569b5d35f18d8a712eda246a4
louaii <louaimisto@gmail.com>
2021-04-29 12:57:51 +0300

    prettify file

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,32 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import TabNavigator from './app/navigation/TabNavigator';
-import { Header } from 'react-native-elements';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./app/navigation/TabNavigator";
+import { Header } from "react-native-elements";
 // import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
-  return <SafeAreaProvider>
-  <NavigationContainer>
-      <Header>
-      {/* <Ionicons name="notifications-circle-outline" size={24} color="black" /> */}
-      </Header>
-      <TabNavigator/>
-    </NavigationContainer>
-  </SafeAreaProvider>
-    // Login space
-    // return (
-    //   <LoginScreen/>
-    // )
-  }
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Header>
+          {/* <Ionicons name="notifications-circle-outline" size={24} color="black" /> */}
+        </Header>
+        <TabNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+  // Login space
+  // return (
+  //   <LoginScreen/>
+  // )
+}
 
-{/* <Header
+{
+  /* <Header
     placement="left"
     // leftComponent={{ icon: 'menu', color: '#fff' }}
     // centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
     rightComponent={{ icon: 'note', color: '#ffff' }}
-    /> */}
+    /> */
+}
