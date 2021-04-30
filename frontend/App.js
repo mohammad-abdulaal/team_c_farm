b3997e53ca59065c84775cc40409ae47f1d282b7
medlij <medlijfatima@gmail.com>
2021-04-30 18:27:29 +0300

    problem solved

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigator from './app/navigation/TabNavigator';
 import { Header } from 'react-native-elements';
@@ -8,36 +8,35 @@ import LoginScreen from './screens/LoginScreen';
 import Splash from './screens/SplashScreen';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SignupScreen from './screens/SignupScreen';
-import Test from './screens/Test';
-// import { Ionicons } from '@expo/vector-icons';
+import Test from './screens/Test'; import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     //<Splash />
 
     //<LoginScreen />
-    <SignupScreen />
-  )/*
+    //<SignupScreen />
+
     <SafeAreaProvider>
       <NavigationContainer>
         <Header>
-          { <Ionicons name="notifications-circle-outline" size={24} color="black" /> }
+          {<Ionicons name="notifications-circle-outline" size={24} color="black" />}
         </Header>
         <TabNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
   );
-  // Login space
-  // return (
-  //   <LoginScreen/>
-  // )
+  //Login space
+  //return (
+  //<LoginScreen/>
+  //)
 }
 
 {
-  /* <Header
+  <Header
     placement="left"
     // leftComponent={{ icon: 'menu', color: '#fff' }}
     // centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
     rightComponent={{ icon: 'note', color: '#ffff' }}
-    /> */
+  />
 }
