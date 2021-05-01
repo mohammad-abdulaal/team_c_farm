b1db00a98b029abb46b00903378b202222dd32be
louaii <louaimisto@gmail.com>
2021-05-01 17:24:00 +0300

    Routing started to work

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Header } from 'react-native-elements';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Header } from "react-native-elements";
 
-import TabNavigator from './app/navigation/TabNavigator';
-import Splash from './screens/SplashScreen';
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import Test from './screens/Test'; import { Ionicons } from '@expo/vector-icons';
+import TabNavigator from "./app/navigation/TabNavigator";
+import Splash from "./screens/SplashScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import Test from "./screens/Test";
+import { Ionicons } from "@expo/vector-icons";
+const Stack = createStackNavigator();
 
-const Stack = createStackNavigator()
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      authenticated: true,
+    }
+  }
 
-
-export default function App() {
+  render () {
   return (
-    //<Splash />
-    <AppStackNavigator/>
-    //<LoginScreen />
-    //<SignupScreen />
-
-    // <SafeAreaProvider>
-    //   <NavigationContainer>
-    //     <Header>
-    //       {<Ionicons name="notifications-circle-outline" size={24} color="black" />}
-    //     </Header>
-    //     <TabNavigator />
-    //   </NavigationContainer>
-    // </SafeAreaProvider>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        {this.state.authenticated ? (
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-  //Login space
-  //return (
-  //<LoginScreen/>
-  //)
+  }
 }
-// {
-//   <Header
-//     placement="left"
-//     // leftComponent={{ icon: 'menu', color: '#fff' }}
-//     // centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-//     rightComponent={{ icon: 'note', color: '#ffff' }}
-//   />
-// }
+
